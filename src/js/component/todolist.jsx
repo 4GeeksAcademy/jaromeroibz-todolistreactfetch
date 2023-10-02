43ae7f599d9dfd967d06fd92941cc748ad86b6b7
jaromeroibz <137736111+jaromeroibz@users.noreply.github.com>
2023-10-02 00:56:49 +0000

    proyeco finalizado

--- src/js/component/todolist.jsx
+++ src/js/component/todolist.jsx
@@ -5,40 +5,87 @@ const Tasks = ()=>{
     const[add, setAdd]= useState('');
     const [hoveredIndex, setHoveredIndex] = useState(null);
 
+        function getTask () {
+          console.log('leer tareas')
+          fetch('https://playground.4geeks.com/apis/fake/todos/user/javierromeror')
+          .then( (response) => response.json() )
+          .then( (data) => console.log(data) )
+        }
+        function createUser () {
+          e.preventDefault();
+          console.log('crear usuario')
+          const requestOptions = {
+            method: 'POST',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify( [] )
+        };
+          fetch('https://playground.4geeks.com/apis/fake/todos/user/javierromeror', requestOptions)
+          .then( (response) => response.json() )
+          .then( (data) => console.log(data) )
+        }
         function addTask (e) {
             e.preventDefault();  
-            // setTasks([...tasks, data]);
-            // setAdd('');      
+            // setAdd('');
+            console.log(add)
+            console.log(tasks)
 
             const requestOptions = {
-                method: 'POST',
-                headers: { 'Content-Type': 'application/json' },
-                body: JSON.stringify({ task: add })
-            };
+              method: 'PUT',
+              headers: { 'Content-Type': 'application/json' },
+              body: JSON.stringify(
+                [
+        
+                  { tasks }
+                  
+                ]
+              )
+          }
+            
+            console.log('Agregar tarea')
+            fetch('https://playground.4geeks.com/apis/fake/todos/user/javierromeror', requestOptions)
+            .then( (response) => response.json() )
+            .then( (data) => console.log(data) )
 
-            fetch('https://playground.4geeks.com/apis/fake/todos/user/javierromero', requestOptions)
-                .then(response => response.json())
-                .then((data) => {console.log(data)})
+            
+          }
 
-        };
+        const deleteTask = (index) => {     
 
-        const deleteTask = (index) => {
             const newList = [...tasks];
             newList.splice(index,1);
             setTasks(newList);
+            
 
             const requestOptions = {
-                method: 'DELETE',
-                headers: { 'Content-Type': 'application/json' },
-                body: JSON.stringify([newList])
+              method: 'PUT',
+              headers: { 'Content-Type': 'application/json' },
+              body: JSON.stringify(
+                
+                 newList
+                
+              )
             };
+            
+            fetch('https://playground.4geeks.com/apis/fake/todos/user/javierromeror', requestOptions)
+            .then( (response) => response.json() )
+            .then( (data) => console.log(data) )
+          
+        }
 
-            fetch('https://playground.4geeks.com/apis/fake/todos/user/javierromero', requestOptions)
-                .then(response => response.json())
-                .then(data => console.log(data));
-
-          };
-
+        const deleteAll = () => {
+      
+          const newList = [];
+          setTasks(newList);
+        
+          const requestOptions = {
+            method: 'DELETE',
+            };
+          
+          fetch('https://playground.4geeks.com/apis/fake/todos/user/javierromeror', requestOptions)
+          .then( (response) => response.json() )
+          .then( (data) => console.log(data) )
+        
+      }
 
         return (
 
@@ -47,7 +94,9 @@ const Tasks = ()=>{
               <h1>To do list</h1>
                 <div>
                   <form onSubmit={addTask}>
-                    <input placeholder="Escribe la tarea" value={add} onChange={(e) => {setAdd(e.target.value);}}/>
+                    <input placeholder="Escribe la tarea" value={add} onChange={(e) => {setAdd(e.target.value)}}/>
+                    <button onClick={() => {setTasks([...tasks, add])}}>Agregar tarea</button>
+
                   </form>
                   <ul>
                     {tasks.map((item,id) => 
@@ -60,10 +109,12 @@ const Tasks = ()=>{
                     </li>
                     )}
                   </ul>
+                  <button onClick={deleteAll}>Delete all tasks and user</button>
                 </div>
            </div>
           </>   
         );
-};
+  };
+
 
 export default Tasks;
